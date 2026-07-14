1ce2940b559ab5a4d10ef9e62c9916146e5bbeea
Ch0p5h0p <samjpoyner@icloud.com>
2026-07-14 18:38:16 -0400

    implement output_frame_notify

--- Makefile
+++ Makefile
@@ -1,18 +1,18 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -std=c11
+CFLAGS = -Wall -Wextra -std=c11 -D_POSIX_C_SOURCE=199309L -DWLR_USE_UNSTABLE
 
-CFLAGS += $(shell pkg-config --cflags wlroots-0.20)
-LDLIBS += $(shell pkg-config --libs wlroots-0.20)
+CFLAGS += $(shell pkg-config --cflags wlroots-0.20 wayland-server)
+LDLIBS += $(shell pkg-config --libs wlroots-0.20 wayland-server) -lrt
 
 .PHONY: all
 .PHONY: clean
 
 all: client server
 
-client: src/client.c
+client:
 	$(CC) $(CFLAGS) src/client.c $(LDLIBS) -o client
 
-server: src/server.c
+server:
 	$(CC) $(CFLAGS) src/server.c $(LDLIBS) -o server
 
 clean:
--- src/server.c
+++ src/server.c
@@ -5,11 +5,17 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include <wayland-server-core.h>
 #include <wlroots-0.20/wlr/backend.h>
 #include <wlroots-0.20/wlr/backend/session.h>
 #include <wlroots-0.20/wlr/types/wlr_output.h>
+#include <wlroots-0.20/wlr/render/pass.h>
+#include <wlroots-0.20/wlr/render/allocator.h>
+#include <wlroots-0.20/wlr/render/wlr_renderer.h>
+
+// TODO: split off output into output.c and output.h. DO NOT DO THIS UNTIL YOU ARE DONE WITH THEM.
 
 struct kwc_server {
     struct wl_display *wl_display;
@@ -20,12 +26,17 @@ struct kwc_server {
 
     struct wl_listener new_output;
     struct wl_list outputs;
+
+    struct wlr_allocator *allocator;
+    struct wlr_renderer *renderer;
 };
 
 struct kwc_output {
+    int width, height;
+
     struct wlr_output *wlr_output;
     struct kwc_server *server;
-    struct timespec lastframe;
+    struct timespec last_frame;
 
     struct wl_listener destroy;
     struct wl_listener frame;
@@ -36,6 +47,43 @@ struct kwc_output {
 static void output_frame_notify(struct wl_listener *listener, void *data) {
     struct kwc_output *output = wl_container_of(listener, output, frame);
     struct wlr_output *wlr_output = data;
+
+    struct wlr_output_state state;
+    wlr_output_state_init(&state);
+
+    struct wlr_render_pass *render_pass = wlr_output_begin_render_pass(wlr_output, &state, NULL);
+
+    if (render_pass == NULL) {
+        wlr_output_state_finish(&state);
+        return;
+    }
+        // Set a clear color, RGBA. Allow this to be configured later
+    struct wlr_render_color clear_color = {
+        .r = 0.1f,
+        .g = 0.1f,
+        .b = 0.1f,
+        .a = 1.0f
+    };
+    wlr_render_pass_add_rect(render_pass, &(struct wlr_render_rect_options){
+        .box = {
+            .x = 0,
+            .y = 0,
+            .width = output->width,
+            .height = output->height,
+        },
+        .color = clear_color,
+        .blend_mode = WLR_RENDER_BLEND_MODE_NONE,
+    });
+
+    if (!wlr_render_pass_submit(render_pass)) {
+        wlr_output_state_finish(&state);
+        return;
+    }
+
+    if (!wlr_output_commit_state(wlr_output, &state)) {
+        // handle commit failure here
+    }
+
 
     // TODO: finish
 }
@@ -77,6 +125,8 @@ static void new_output_notify(struct wl_listener *listener, void *data) {
     output->wlr_output = wlr_output;
     wl_list_insert(&server->outputs, &output->link);
 
+    wlr_output_transformed_resolution(wlr_output, &output->width, &output->height);
+
     output->destroy.notify = output_destroy_notify;
     wl_signal_add(&wlr_output->events.destroy, &output->destroy);
     output->frame.notify = output_frame_notify;
@@ -95,6 +145,11 @@ int main(int argc, char *argv[]) {
     assert(server.session);
     server.backend = wlr_backend_autocreate(server.wl_event_loop, &server.session);
     assert(server.backend);
+
+    server.renderer = wlr_renderer_autocreate(server.backend);
+    assert(server.renderer);
+    server.allocator = wlr_allocator_autocreate(server.backend, server.renderer);
+    assert(server.allocator);
 
     wl_list_init(&server.outputs);
 
